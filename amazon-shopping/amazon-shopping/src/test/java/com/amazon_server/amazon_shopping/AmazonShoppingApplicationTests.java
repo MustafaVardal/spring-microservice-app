52bdf352707f4b52be4e9b5f3675bcd1a3e1de89
MustafaVardal <m.vardal@hotmail.com>
2025-03-20 09:26:44 -0400

    Need Review

---
+++ amazon-shopping/amazon-shopping/src/test/java/com/amazon_server/amazon_shopping/AmazonShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amazon_server.amazon_shopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonShoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
